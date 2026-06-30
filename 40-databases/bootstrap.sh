6d99012a952ba836b40f213d40a884b1dc2de7f3
balaji <balubongu185@gmail.com>
2026-07-01 03:10:57 +0530

    roboshop dev infra

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 component=$1
-dnf ansible install -y
-ansible-pull https://github.com/pattasai123/ansi_roles_tf.git -e component=$component main.yaml
+
+dnf install -y ansible
+
+ansible-pull \
+  -U https://github.com/pattasai123/ansi_roles_tf.git \
+  -e component=$component \
+  main.yaml
