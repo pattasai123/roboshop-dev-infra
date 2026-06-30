cc496e028cae1ec850074fa05ef1b81efadcb305
balaji <balubongu185@gmail.com>
2026-07-01 02:22:42 +0530

    roboshop dev infra

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "mongodb_host" {
     var.bastion_tags,
     local.common_tags,
     {
-      Name = local.common_name
+      Name = "${local.common_name}-mongodb"
     }
   )
 }
@@ -26,15 +26,98 @@ resource "aws_instance" "mongodb_host" {
     password    = "DevOps321"
     host        = aws_instance.mongodb_host.private_ip
   }
-   provisioner "file" {
+  provisioner "file" {
     source      = "bootstrap.sh"             # Path on your local machine
-    destination = "/var/bootstrap.sh"     # Path on the remote server
+    destination = "/tmp/bootstrap.sh"     # Path on the remote server
   }
   provisioner "remote-exec" {
     
     inline = [
-      "chmod +x /var/bootstrap.sh",
-      "sudo sh /var/bootstrap.sh"
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh"
     ]
   }
 }
+
+/*
+resource "aws_instance" "redis_host" {
+  ami                    = local.ami
+  instance_type          = var.instance_type
+
+  subnet_id              = local.subnet[0]
+  vpc_security_group_ids = [local.redis_sg_id]
+
+  tags = merge(
+    var.bastion_tags,
+    local.common_tags,
+    {
+      Name = local.common_name
+    }
+  )
+}
+
+ resource "null_resource" "cluster" {
+ 
+  triggers = {
+    instance_id = aws_instance.redis_host.id
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    password    = "DevOps321"
+    host        = aws_instance.redis_host.private_ip
+  }
+  provisioner "file" {
+    source      = "bootstrap.sh"             # Path on your local machine
+    destination = "/tmp/bootstrap.sh"     # Path on the remote server
+  }
+  provisioner "remote-exec" {
+    
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh"
+    ]
+  }
+}
+resource "aws_instance" "rabbitmq_host" {
+  ami                    = local.ami
+  instance_type          = var.instance_type
+
+  subnet_id              = local.subnet[0]
+  vpc_security_group_ids = [local.rabbitmq_sg_id]
+
+  tags = merge(
+    var.bastion_tags,
+    local.common_tags,
+    {
+      Name = local.common_name
+    }
+  )
+}
+
+ resource "null_resource" "cluster" {
+ 
+  triggers = {
+    instance_id = aws_instance.rabbitmq_host.id
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    password    = "DevOps321"
+    host        = aws_instance.rabbitmq_host.private_ip
+  }
+   provisioner "file" {
+    source      = "bootstrap.sh"             # Path on your local machine
+    destination = "/tmp/bootstrap.sh"     # Path on the remote server
+  }
+  provisioner "remote-exec" {
+    
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh"
+    ]
+  }
+}
+*/
