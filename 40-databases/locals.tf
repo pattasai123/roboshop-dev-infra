16c97f10b9d38419e2d956630636a5e5dd29b00b
balaji <balubongu185@gmail.com>
2026-07-02 19:21:20 +0530

    roboshop dev infra

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -4,6 +4,7 @@ locals {
   redis_sg_id    = data.aws_ssm_parameter.redis_sg_id.value
 
   rabbitmq_sg_id    = data.aws_ssm_parameter.rabbitmq_sg_id.value
+  mysql_sg_id    = data.aws_ssm_parameter.mysql_sg_id.value
    subnet=[
     data.aws_ssm_parameter.database_subnet_a.value,
     data.aws_ssm_parameter.database_subnet_b.value
