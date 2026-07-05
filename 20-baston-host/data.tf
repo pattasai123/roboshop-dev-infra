37ef13b1d25e57cf50b09b63fe738302431c4935
balaji <balubongu185@gmail.com>
2026-07-05 20:19:35 +0530

    roboshop dev infra

--- 20-baston-host/data.tf
+++ 20-baston-host/data.tf
@@ -9,3 +9,7 @@ data "aws_ssm_parameter" "public_subnet_a" {
 data "aws_ssm_parameter" "bastion_sg_id" {
   name = "/${var.project}/${var.environment}/bastion_sg_id"
 }
+
+data "aws_iam_role" "bastion" {
+  name = "EC2bastion"
+}
--- 20-baston-host/main.tf
+++ 20-baston-host/main.tf
@@ -1,3 +1,8 @@
+resource "aws_iam_instance_profile" "bastion" {
+  name = "bastion"
+  role = data.aws_iam_role.bastion.name
+}
+
 resource "aws_instance" "bastion-host" {
   ami                    = local.ami
   instance_type          = var.instance_type
@@ -5,6 +10,7 @@ resource "aws_instance" "bastion-host" {
   subnet_id              = data.aws_ssm_parameter.public_subnet_a.value
   vpc_security_group_ids = [local.bastion_sg_id]
   user_data=file("bootstrap.sh")
+  iam_instance_profile   = aws_iam_instance_profile.bastion.name
   tags = merge(
     var.bastion_tags,
     local.common_tags,
