3f02758eaad2e5bd756b172c5e5abba96b7542d0
balaji <balubongu185@gmail.com>
2026-07-13 20:51:53 +0530

    roboshop dev infra

--- 70-acm/parameters.tf
+++ 70-acm/parameters.tf
@@ -1,4 +1,5 @@
 resource "aws_ssm_parameter" "fronted_alb_arn" {
-  name  = "/${var.project}/${var.env}/fronted_alb_arn
+  name  = "/${var.project}/${var.env}/fronted_alb_arn"
+  type  = "String"
   value = aws_acm_certificate.roboshop.arn
 }
