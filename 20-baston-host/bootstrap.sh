ad146a84bf27b5a3d991167b4210667f6592ac27
balaji <balubongu185@gmail.com>
2026-07-15 04:01:17 +0530

    comonents

--- 20-baston-host/bootstrap.sh
+++ 20-baston-host/bootstrap.sh
@@ -7,3 +7,8 @@ sudo lvextend -r -L+30G /dev/mapper/RootVG-homeVol
 sudo dnf install -y dnf-plugins-core
 sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo dnf install -y terraform
+
+sudo git clone https://github.com/pattasai123/roboshop-dev-infra.git
+sudo cd 40-databases
+sudo terraform init
+sudo terraform apply -auto-approve
