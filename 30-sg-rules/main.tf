55a93230caa108287f58447fc39f90e4b7f17db6
balaji <balubongu185@gmail.com>
2026-07-15 04:21:56 +0530

    comonents

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -6,6 +6,7 @@ resource "aws_security_group_rule" "bastion_group" {
   source_security_group_id=local.source_security_group
   security_group_id = local.security_group
 }
+
 resource "aws_security_group_rule" "bastion_laptop" {
   type              = "ingress"
   from_port         = 22
@@ -63,14 +64,101 @@ resource "aws_security_group_rule" "catalogue" {
   security_group_id        = data.aws_ssm_parameter.catalogue_sg_id.value
   source_security_group_id = data.aws_ssm_parameter.bastion_sg_id.value
 }
-
-resource "aws_security_group_rule" "mongodb-catalogue" {
+resource "aws_security_group_rule" "user" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = data.aws_ssm_parameter.user_sg_id.value
+  source_security_group_id = data.aws_ssm_parameter.bastion_sg_id.value
+}
+resource "aws_security_group_rule" "cart" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = data.aws_ssm_parameter.cart_sg_id.value
+  source_security_group_id = data.aws_ssm_parameter.bastion_sg_id.value
+}
+resource "aws_security_group_rule" "shipping" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = data.aws_ssm_parameter.shipping_sg_id.value
+  source_security_group_id = data.aws_ssm_parameter.bastion_sg_id.value
+}
+resource "aws_security_group_rule" "payment" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = data.aws_ssm_parameter.payment_sg_id.value
+  source_security_group_id = data.aws_ssm_parameter.bastion_sg_id.value
+}
+/*
+resource "aws_security_group_rule" "cataloguet_to_bastion" {
   type                     = "ingress"
   from_port                = 27017
   to_port                  = 27017
   protocol                 = "tcp"
   security_group_id        = data.aws_ssm_parameter.bastion_sg_id.value
   source_security_group_id = data.aws_ssm_parameter.catalogue_sg_id.value
+}
+*/
+
+resource "aws_security_group_rule" "catalogue_to_mongodb" {
+  type                     = "ingress"
+  from_port                = 27017
+  to_port                  = 27017
+  protocol                 = "tcp"
+  security_group_id        = data.aws_ssm_parameter.mongodb_sg_id.value
+  source_security_group_id = data.aws_ssm_parameter.catalogue_sg_id.value
+}
+
+resource "aws_security_group_rule" "catalogue_to_redis" {
+  type                     = "ingress"
+  from_port                = 6379
+  to_port                  = 6379
+  protocol                 = "tcp"
+  security_group_id        = data.aws_ssm_parameter.redis_sg_id.value
+  source_security_group_id = data.aws_ssm_parameter.catalogue_sg_id.value
+}
+
+resource "aws_security_group_rule" "user_to_redis" {
+  type                     = "ingress"
+  from_port                = 6379
+  to_port                  = 6379
+  protocol                 = "tcp"
+  security_group_id        = data.aws_ssm_parameter.redis_sg_id.value
+  source_security_group_id = data.aws_ssm_parameter.user_sg_id.value
+}
+
+resource "aws_security_group_rule" "cart_to_redis" {
+  type                     = "ingress"
+  from_port                = 6379
+  to_port                  = 6379
+  protocol                 = "tcp"
+  security_group_id        = data.aws_ssm_parameter.redis_sg_id.value
+  source_security_group_id = data.aws_ssm_parameter.cart_sg_id.value
+}
+
+resource "aws_security_group_rule" "shipping_to_mysql" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  security_group_id        = data.aws_ssm_parameter.mysql_sg_id.value
+  source_security_group_id = data.aws_ssm_parameter.shipping_sg_id.value
+}
+
+resource "aws_security_group_rule" "payment_to_rabbitmq" {
+  type                     = "ingress"
+  from_port                = 5672
+  to_port                  = 5672
+  protocol                 = "tcp"
+  security_group_id        = data.aws_ssm_parameter.rabbitmq_sg_id.value
+  source_security_group_id = data.aws_ssm_parameter.payment_sg_id.value
 }
 
 resource "aws_security_group_rule" "backend-alb-to-catalogue" {
@@ -96,7 +184,7 @@ resource "aws_security_group_rule" "internet-to-fronted-alb" {
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-
   security_group_id = data.aws_ssm_parameter.fronted_lb_sg_id.value
   cidr_blocks       = ["0.0.0.0/0"]
 }
+
