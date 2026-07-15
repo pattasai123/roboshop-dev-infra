e2705c363bb8862c076cb17b6875bc5f02d0ae90
balaji <balubongu185@gmail.com>
2026-07-15 20:20:34 +0530

    roboshop dev infra

--- 20-baston-host/bootstrap.sh
+++ 20-baston-host/bootstrap.sh
@@ -8,8 +8,10 @@ sudo dnf install -y dnf-plugins-core
 sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo dnf install -y terraform
 
+set -ex
+dnf install -y git
 git clone https://github.com/pattasai123/roboshop-dev-infra.git
-cd 40-databases
+cd roboshop-dev-infra/40-databases
 terraform init -reconfigure
 terraform apply -auto-approve
 cd ..
