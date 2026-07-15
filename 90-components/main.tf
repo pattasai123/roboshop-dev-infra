059ae251c00a54c2176c62caab8d40bbbb573d9f
balaji <balubongu185@gmail.com>
2026-07-15 20:51:27 +0530

    roboshop dev infra

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,7 +1,7 @@
 module "component"{
   source="../../terraform-roboshop-dev-components"
   components=var.components
-  instance="t3.micro"
+  instance_type="t3.micro"
   project=var.project
   env=var.env
   domain_name=var.domain_name
