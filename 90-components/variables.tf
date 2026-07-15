205a87fa705bd0acb9c115f473664294336e039d
balaji <balubongu185@gmail.com>
2026-07-15 20:23:15 +0530

    roboshop dev infra

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -1,19 +1,18 @@
 variable "project"{
     default="roboshop"
-    type="String"
 }
 
 variable "env"{
     default="dev"
-    type="String"
+
 }
 
 variable "domain_name"{
     default="bongu.online"
-    type="String"
+
 }
 
 variable "components"{
     default="catalogue"
-    type="String"
+
 }
