c60fe7eda58b79c4367f44322c9d8ac36813297c
balaji <balubongu185@gmail.com>
2026-07-16 20:23:46 +0530

    roboshop dev infra

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -2,8 +2,9 @@ module "component"{
   source="git::https://github.com/pattasai123/terraform-roboshop-dev-components.git"
   for_each=var.components
   components=each.key
-  instance_type="t3.micro"
   priority=each.value
+  
+  instance_type="t3.micro"
   project=var.project
   env=var.env
   domain_name=var.domain_name
